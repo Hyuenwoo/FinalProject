e1c65dd3e737d3e35128c1b50d058ac4337e9ac5
Hyuenwoo <39932900+Hyuenwoo@users.noreply.github.com>
2018-12-03 16:32:20 +0900

    작물정보추천화면

--- app/src/main/java/com/example/hwhan/finalproject/CropData.java
+++ app/src/main/java/com/example/hwhan/finalproject/CropData.java
@@ -37,13 +37,6 @@ public class CropData {
 }*/
 
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 public class CropData extends AppCompatActivity {
 
     @Override
